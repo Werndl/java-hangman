d413c5b231c84d4429d0ca54c5d165799f0f5235
Grzegorz Wolski <gwolski93@gmail.com>
2020-03-01 12:08:24 +0100

    Guessing function update

--- src/main/java/pl/edu/agh/hangman/Guessing.java
+++ src/main/java/pl/edu/agh/hangman/Guessing.java
@@ -4,26 +4,44 @@ import javax.swing.*;
 
 public class Guessing {
 
-    public static void Checking(String[] passwordWord, String checkingLater ){
+    public static void Checking(String[] passwordWord, String checkingLetter){
 
+        int howManyIncorrect = passwordWord.length;
         for(int i =0; passwordWord.length<i;i++){
 
-            if(passwordWord[i] == checkingLater){
-                // good(); pozniejsza implementacja
-                else
-                    // bad(); pozniejsza impleementacja
+            if(passwordWord[i].equals(checkingLetter)){
+                changeArray();
+                break;
+                else{
+                    howManyIncorrect--;
+                }
+
+            }
+            if (howManyIncorrect==0){
+                System.out.println("Nie ma takiej litery w slowie");
             }
         }
+        showResults();
 
     }
 
-    public static String[] table___(String[] passwordWord){
+    public static String[] table_(String[] passwordWord){
         String[] tableWith_ = passwordWord;
         for (int i = 0; passwordWord.length<i;i++) {
-            tableWith[i] = "_";
+            tableWith_[i] = "_";
         }
 
         return tableWith_;
+    }
+
+    public static void showResults(){
+        System.out.println(final)
+
+    }
+
+
+    public static void changeArray(){
+
     }
 
 }
